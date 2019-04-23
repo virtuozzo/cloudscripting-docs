000b33732f6586d938865c074fed5169a211530d
alexey.lazarenko <alexey.lazarenko@jelastic.com>
2019-04-23 15:07:59 +0300

    JE-46675 [[CS] Docs improvements] [Build] - fix versions order

--- 3rdparty/mkdocs-versioned/mkdocs_versioned/cli.py
+++ 3rdparty/mkdocs-versioned/mkdocs_versioned/cli.py
@@ -171,4 +171,4 @@ def build_command(config_file, strict, site_dir, branches, default_branch, lates
     print("Checkout branch %s", active_branch)
 
     g.checkout(active_branch)
-    # repo.git.stash("pop")
+    repo.git.stash("apply")
